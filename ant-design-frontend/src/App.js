8b65eed3ed2c16ef5b202f4c88463869be3d1e63
Sdsdsde2 <imspammails31@gmail.com>
2021-07-31 15:31:25 -0500

    added example form UI with styling.

--- ant-design-frontend/src/App.js
+++ ant-design-frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Layout, Menu, PageHeader, Avatar, Form, Input, Button } from 'antd';
+import { Layout, Menu, PageHeader, Avatar, Form, Input, Switch, Button, Divider } from 'antd';
 import { AntDesignOutlined } from '@ant-design/icons';
 
 const { Header, Content } = Layout;
@@ -36,6 +36,67 @@ function App() {
             icon={<AntDesignOutlined />}
             className="avatar"
           />
+
+          <Form
+            name="basic"
+            layout="vertical"
+            style={{ 
+              float: 'left',
+              paddingLeft: '20px',
+              width: "275px" 
+            }}
+          >
+
+            <Form.Item
+              label="Full name"
+              name="fullname"
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Email"
+              name="email"
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Timezone"
+              name="timezone"
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item label="Preferences">
+              <p style={{width: "350px", marginBottom: "-10px", paddingLeft: "55px", textAlign: "left"}}>Notify me when tasks are updated</p>
+              <Switch style={{float: "left"}}/>
+              <p style={{marginTop: "-10px", width: "500px", float: "left", fontSize: "12px", paddingLeft: "55px", textAlign: "left", color: "grey"}}>Toggle on to receive notifications in your email when tasks are updated</p>
+              <Divider style={{borderTop: "2px solid grey", opacity: "30%"}}/>
+            </Form.Item>
+
+            <Form.Item style={{marginTop: "-45px"}}>
+              <p style={{width: "350px", marginBottom: "-10px", paddingLeft: "55px", textAlign: "left"}}>Notify me when projects are updated</p>
+              <Switch style={{float: "left"}}/>
+              <p style={{marginTop: "-10px", width: "500px", float: "left", fontSize: "12px", paddingLeft: "55px", textAlign: "left", color: "grey"}}>Toggle on to receive notifications in your email when projects are updated</p>
+              <Divider style={{borderTop: "2px solid grey", opacity: "30%"}}/>
+            </Form.Item>
+
+            <Form.Item style={{marginTop: "-45px"}}>
+              <p style={{width: "350px", marginBottom: "-10px", paddingLeft: "55px", textAlign: "left"}}>Notify me when client tasks are updated</p>
+              <Switch style={{float: "left"}}/>
+              <p style={{marginTop: "-10px", width: "500px", float: "left", fontSize: "12px", paddingLeft: "55px", textAlign: "left", color: "grey"}}>Toggle on to receive notifications in your email when client tasks are updated</p>
+              <Divider style={{borderTop: "2px solid grey", opacity: "30%"}}/>
+            </Form.Item>
+
+            <Form.Item style={{marginTop: "-45px"}}>
+              <p style={{width: "350px", marginBottom: "-10px", paddingLeft: "55px", textAlign: "left"}}>Dark Mode</p>
+              <Switch style={{float: "left"}}/>
+              <p style={{marginTop: "-10px", width: "500px", float: "left", fontSize: "12px", paddingLeft: "55px", textAlign: "left", color: "grey"}}>Toggle between light and dark mode</p>
+              <Divider style={{borderTop: "2px solid grey", opacity: "30%"}}/>
+            </Form.Item>
+
+          </Form>
         </Content>
       </Layout>
     </div>

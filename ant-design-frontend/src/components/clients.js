f265006e33f6a7c29b6a52c687bd3d1d9d086395
Sdsdsde2 <imspammails31@gmail.com>
2021-08-28 19:16:28 -0500

    installed ant-design/icons. added search and create new client dropdown to client page. styled search and dropdown.

--- ant-design-frontend/src/components/clients.js
+++ ant-design-frontend/src/components/clients.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
-import { PageHeader, Table } from 'antd';
+import { PageHeader, Table, Input, Menu, Dropdown, Button, Modal } from 'antd';
+import { SearchOutlined, CaretDownOutlined } from '@ant-design/icons'
 
+const menu = (
+    <Menu>
+        <Menu.Item key="1">
+            Business
+        </Menu.Item>
+        <Menu.Item key="2">
+            Personal
+        </Menu.Item>
+    </Menu>
+)
+
+// TODO: Change out this 'data' array with client info from backend
 const data = [
     {
         key: 1,
@@ -50,7 +63,27 @@ const columns = [
     },
 ];
 
+const businessModal = (
+    <Modal
+        title="Create a client (Business)"
+    >
+        <p>Hello Modal</p>
+    </Modal>
+)
+
+const personalModal = (
+    <Modal
+        title="Create a client (Personal)"
+    >
+        <p>Hello Modal</p>
+    </Modal>
+)
+
 export default class Clients extends Component {
+    constructor(props) {
+        super();
+
+    }
 
     render() {
         return (
@@ -59,6 +92,21 @@ export default class Clients extends Component {
                     className="site-page-header"
                     title="Clients"
                 />
+
+                <Input 
+                    className="ant-client-search"
+                    placeholder="Search clients"
+                    prefix={ <SearchOutlined /> }
+                    onSearch={console.log("searching")}
+                />
+
+                <Dropdown overlay={menu}>
+                    <Button className="ant-create-client">
+                        Create Client <CaretDownOutlined />
+                    </Button>
+                </Dropdown>
+
+                businessModal
 
                 <Table columns={columns} dataSource={data} />
             </div>

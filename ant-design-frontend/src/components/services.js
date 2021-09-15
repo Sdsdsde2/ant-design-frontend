c07c896f8ccbd6c8945ab4725b6844196174eace
Sdsdsde2 <imspammails31@gmail.com>
2021-09-14 23:03:41 -0500

    added Table component for Service list.

--- ant-design-frontend/src/components/services.js
+++ ant-design-frontend/src/components/services.js
@@ -1,6 +1,40 @@
 import React, { Component } from 'react'
 import { PageHeader, Typography, Table, Input, Menu, Dropdown, Button, Modal, Steps } from 'antd';
-import { SearchOutlined, CaretDownOutlined } from '@ant-design/icons'
+import { SearchOutlined } from '@ant-design/icons'
+
+// TODO: Change out this 'data' array with services info from backend
+const data = [
+    // {
+    //     key: 7,
+    //     client: 'Ant Design Title 1',
+    //     email: 'fakeemail@gmail.com'
+    // },
+    // {
+    //     key: 8,
+    //     client: 'Ant Design Title 2',
+    //     email: 'fakeemail@gmail.com'
+    // },
+    // {
+    //     key: 9,
+    //     client: 'Ant Design Title 3',
+    //     email: 'fakeemail@gmail.com'
+    // }
+];
+
+const columns = [
+    {
+      title: 'SERVICE NAME',
+      dataIndex: 'service',
+      key: 'service',
+      width: '65%',
+    },
+    {
+      title: 'ACTIONS',
+      dataIndex: 'actions',
+      key: 'actions',
+      width: '20%',
+    }
+];
 
 export default class Services extends Component {
     constructor(props) {
@@ -35,13 +69,17 @@ export default class Services extends Component {
                         Create Client
                 </Button>
 
-                <Typography className="ant-empty-services-label">
-                        You have not created any services yet!
-                </Typography>
+                <div>
+                    <Typography className="ant-empty-services-label">
+                            You have not created any services yet!
+                    </Typography>
 
-                <p>
-                    Click the "Create Service" button to get started.
-                </p>
+                    <p>
+                        Click the "Create Service" button to get started.
+                    </p>
+                </div>
+
+                <Table columns={columns} dataSource={data} />
           </div>
         )
     }
